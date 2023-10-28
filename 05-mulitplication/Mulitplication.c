c547f1c21d606b0523aa83ef9b2360936c25b399
RautMansi <148704592+RautMansi@users.noreply.github.com>
2023-10-28 20:03:25 +0530

    Update Mulitplication.c

--- 05-mulitplication/Mulitplication.c
+++ 05-mulitplication/Mulitplication.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int m=30;
+  int n=10;
+  int multi=0;
 
+  multi=m*n;
+  printf("m value=%d/n",m);
+  printf("n value=%d/n",n);
+  printf("multi value=%d/n",multi);
+
+  printf("m address=%d/n",&m);
+  printf("n address=%d/n",&n);
+  printf("multi address=%d/n",&multi);
+  return 0;
+}
