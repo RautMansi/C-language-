be58958ee5bc3f4e875e668f9538e14c3c3e1633
RautMansi <148704592+RautMansi@users.noreply.github.com>
2023-10-28 19:17:18 +0530

    Update Divison.c

--- 04-Divison/Divison.c
+++ 04-Divison/Divison.c
@@ -1,1 +1,19 @@
+//write a code for divison of two numbers
+//solution:-
+# include<stdio.h>
+int main()
+{
+  int x=50;
+  int y=20;
+  int div=0;
+
+  printf("x value=%d/n",x);
+  printf("y value=%d/n",y);
+  printf("div value=%d/n",div);
+
+  printf("x address=%d/n",&x);
+  printf("y address=%d/n",&y);
+  printf("div address=%d/n",&div);
+  return 0;
+}
 
