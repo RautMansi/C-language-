6355f6c887c5f5d7154516a3169024eb6612f533
RautMansi <148704592+RautMansi@users.noreply.github.com>
2023-10-28 19:49:39 +0530

    Update Arethmatic-operator.c

--- 06-Arethmatic operator/Arethmatic-operator.c
+++ 06-Arethmatic operator/Arethmatic-operator.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int a=8,b=3,c=0;
+  double d=0.0;
 
+  c=a+b;
+  printf("a+b=%d/n",c);
+
+  c=a-b;
+  printf("a-b=%d/n",c);
+
+  c=a*b;
+  printf("a*b=%d/n",c);
+
+  d=a/(float)b;
+  printf("a/b=%2f/n",d);
+ 
+  c=a%b;
+  printf("remainder when a dividend by=%d/n",c);
+}
