da3baf83f30bc80b896c852f652fddddce25a64f
RautMansi <148704592+RautMansi@users.noreply.github.com>
2023-10-28 18:50:16 +0530

    Update Data -type.c

--- 02-Data type/Data -type.c
+++ 02-Data type/Data -type.c
@@ -1,1 +1,12 @@
-
+//write code for data size
+//solution:-
+#include<stdio.h>
+int main()
+{
+ printf("the size of integer datatype is %d/n",sizeof(int));
+ printf(" the size of character datatype is %d/n",sizeof(char));
+ printf("the size of double datstype is %d/n",sizeof(double));
+ printf("the value of long integer datatype is %d/n", sizeof(long integer));
+ printf("the value of short datatype is %d/n",sizeof(short));
+ printf("the value of long double is %d/n",sizeof(long double));
+ printf("the value of float datatype is %d/n",sizeof(float));
