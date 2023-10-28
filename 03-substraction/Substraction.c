6b91210e8f65aafa7db3d2925918859c75067e72
RautMansi <148704592+RautMansi@users.noreply.github.com>
2023-10-28 19:09:40 +0530

    Update Substraction.c

--- 03-substraction/Substraction.c
+++ 03-substraction/Substraction.c
@@ -1,1 +1,18 @@
+write code for substraction of two number
+solution:-
+  #include<stdio.h>
+  int main()
+  {
+    int p=40;
+    int q=20;
+    int sub=0;
+    sub=p-q;
+    printf("p value %d/n",p);
+    printf("q value %d/n",q);
+    printf("sub value %d/n",sub);
 
+   printf("p address=%d/n",&p);
+   printf("q address=%d/n",&q);
+   printf("sub address=%d/n",sub);
+   return 0;
+}
