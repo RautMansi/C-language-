cfa5f00db56bb3eab94b1610cd7a17f606e01be6
RautMansi <148704592+RautMansi@users.noreply.github.com>
2023-10-29 21:39:53 +0530

    Update 01-decrement.c

--- 07-increment & decrement/01-decrement.c
+++ 07-increment & decrement/01-decrement.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+void main()
+{
+    int a = 0, b = 10;
+
+    
+    printf("Post Decrement = %d \n",b--);
+    printf("After Post Decrement = %d \n",b);
+    printf("Pre Decrement = %d \n",--b);
+    return 0;
+}
+
+
+
